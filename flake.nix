9c9f0885285c9b2d32f203ee0de8e85107ba6fe8
Wes Payne <noblepayne@noblepayne.com>
2024-12-29 17:23:22 -0800

    initial flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Precompiled boltz-client flake.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux"];
+    pkgsBySystem = nixpkgs.lib.getAttrs supportedSystems nixpkgs.legacyPackages;
+    forAllPkgs = fn: nixpkgs.lib.mapAttrs (system: pkgs: (fn pkgs)) pkgsBySystem;
+    version = "2.3.4";
+
+    boltzClientFor = pkgs:
+      pkgs.stdenv.mkDerivation {
+        inherit version;
+        pname = "boltz-client";
+
+        src = pkgs.fetchzip {
+          url = "https://github.com/BoltzExchange/boltz-client/releases/download/v2.3.4/boltz-client-linux-amd64-v2.3.4.tar.gz";
+          hash = "sha256-HcPlxsDoxXn9NS7KR3+3Q/q/Lsj+dWMEyFemQ0vStwg=";
+        };
+
+        nativeBuildInputs = with pkgs; [
+          autoPatchelfHook
+        ];
+
+        buildInputs = with pkgs; [
+          stdenv.cc.cc.lib # This provides libstdc++
+        ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp **/* $out/bin/
+        '';
+      };
+  in {
+    formatter = forAllPkgs (pkgs: pkgs.alejandra);
+    packages = forAllPkgs (pkgs: {
+      default = boltzClientFor pkgs;
+    });
+  };
+}
